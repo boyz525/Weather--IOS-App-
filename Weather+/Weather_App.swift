ee5eb660127a8444199e6707a0b51f08afc49cf3
boyzcomzretr <ballin.alex@ya.ru>
2026-04-08 18:15:21 +0300

    Initial Commit

---
+++ Weather+/Weather_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App.swift
+//  Weather+
+//
+//  Created by Александр Малахов on 08.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
